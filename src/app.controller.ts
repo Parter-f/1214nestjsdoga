988b6c31dc98a3e4a952e4511165b4c1acb0f1c0
Árpás <arpaspeter0@gmail.com>
2022-12-14 13:19:51 +0100

    új macska form kész és működik

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,9 @@ import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 import db from './db';
 import { CatsDto } from './cats.dto';
+import { Post } from '@nestjs/common';
+import { Redirect } from '@nestjs/common';
+import { Body } from '@nestjs/common';
 
 @Controller()
 export class AppController {
@@ -17,5 +20,20 @@ export class AppController {
     return {
       macskak: rows,
     };
+  }
+
+  @Get('cats/new')
+  @Render('form')
+  newMacskakForm() {
+    return {};
+  }
+  @Post('cats/new')
+  @Redirect()
+  async newMacska(@Body() macska: CatsDto) {
+    const [result]: any = await db.execute(
+      'INSERT INTO macska (suly , szem_szin) VALUES (? , ?)',
+      [macska.suly, macska.szem_szin],
+    );
+    return { url: '/' };
   }
 }
